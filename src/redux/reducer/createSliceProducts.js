813dbfb51041f83ccc67362369e1e22fbab332a4
Mohamed Makawy <80915451+makawy390@users.noreply.github.com>
2023-12-04 23:03:00 +0200

    Update createSliceProducts.js

--- src/redux/reducer/createSliceProducts.js
+++ src/redux/reducer/createSliceProducts.js
@@ -7,12 +7,14 @@ const cartSlice = createSlice({
     name: "products",
     initialState,
     reducers : {
-        addCart : (state , action)=>{
-         const id = action.payload.id;
-         state.count =+1;
-         const cartItem = state.cart.find(cart => cart._id === id);
-         cartItem ? (cartItem.count  =+1) : state.cart.push({...action.payload , count : 1})
-        },
+       addCart: (state, action) => {
+      const id = action.payload._id;
+      state.count = +1;
+      const cartItem = state.cart.findIndex((cart) => cart._id === id);
+      cartItem >= 0
+        ? (state.cart[cartItem].count += 1) //(cartItem.count = +1)
+        : state.cart.push({ ...action.payload, count: 1 });
+    },
         deleteCart : (state , action)=>{
             state.cart = state.cart.filter((item) => item.id !== action.payload)
             state.count -= 1;
